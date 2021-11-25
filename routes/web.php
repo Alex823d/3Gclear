bf9c6dee6b443d862145b3047ddf5c530290049e
giorgi <gbaxbaia@gmail.com>
2021-11-25 12:56:34 +0400

    update

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get(config('cms.front.coming_soon_prefix'),function(){
-    return view('coming');
-});
 
-Route::middleware(['auth:sanctum', 'verified','language'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
-
-Route::get('/fire', function () {
-    event(new \App\Events\TestEvent());
-    return 'ok';
-});
