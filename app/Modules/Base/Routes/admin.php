58bb25d9208310ea01a80d3aea3bab8de9080a56
Alexander <92953068+Alex823d@users.noreply.github.com>
2021-11-25 14:29:18 +0400

    Delete app/Modules/Base directory

--- app/Modules/Base/Routes/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-Route::name(config('cms.admin.base_route_name') . '.')->group(function(){
-    Route::name('image.')->prefix('images')->group(function(){
-        Route::post('/upload',  'ImageController@uploadImage')->name('upload');
-    });
-});
