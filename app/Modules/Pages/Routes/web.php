ca6f38de9801cc2e46e4baa9ed952c272213cc21
giorgi <gbaxbaia@gmail.com>
2021-11-25 13:02:45 +0400

    update

--- app/Modules/Pages/Routes/web.php
+++ app/Modules/Pages/Routes/web.php
@@ -2,10 +2,7 @@
 
 Route::namespace('Api\Client')->group(function(){
     Route::get('localization-text','LocalizationController@getLangFile');
-    Route::prefix('static-page')->group(function(){
-        Route::get('{module}','StaticPageController@index');
-        Route::get('{module}/show/{id}','StaticPageController@show');
-    });
+
     Route::prefix('page')->name('page.')->group(function(){
         Route::get('{module}','PageController@index')->name('index');
         Route::get('type','PageController@pageType');
